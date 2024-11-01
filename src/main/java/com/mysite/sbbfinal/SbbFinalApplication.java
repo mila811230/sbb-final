400376fd52004da793cee15f58678821aafac388
mila811230 <bluegalaxy1230@gmail.com>
2024-11-01 09:40:15 +0900

    프로젝트시작

---
+++ src/main/java/com/mysite/sbbfinal/SbbFinalApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sbbfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sbbfinal/SbbFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbbfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
